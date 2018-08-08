934f340cb8e7d653d5a3a200a09f02ccb8b52ea7
Linus Ramneborg <uberhaxor101@live.com>
2018-08-09 01:50:02 +0200

    Add emote parser and general cleanup
    Also, server names can contain emotes. :ballot_box_with_check:

--- chat.lua
+++ chat.lua
@@ -3,7 +3,7 @@ local username = "Guest"..math.random(0,10000)
 if tArgs[1] ~= nil and type(tArgs[1]) == "string" then
   username = tArgs[1]
 end
-local version = "beta v1.5"
+local version = "beta v1.5 experimental"
 os.loadAPI(".chat/net")
 net.disablelog()
 local mx, my = term.getSize()
@@ -34,16 +34,67 @@ local attempt = 1
 local status = "Connecting.."
 local select = 1
 local mx, my = term.getSize()
+
+function emoteParse(input)
+  local parsed_string = ""
+  local string_separated = {}
+  -- Separate string into table on a per word basis
+  for word in input:gmatch("%S+") do table.insert(string_separated, word) end
+
+  -- Iterate through the table, replace items which are known emotes
+  for k,v in ipairs(string_separated) do
+    if v == ":heart:" then
+      string_separated[k] = "\3"
+    end
+    if v == ":fem:" then
+      string_separated[k] = "\12"
+    end
+    if v == ":mal:" then
+      string_separated[k] = "\11"
+    end
+    if v == ":smile:" then
+      string_separated[k] = "\2"
+    end
+    if v == ":double_exclaim:" then
+      string_separated[k] = "\19"
+    end
+    if v == ":mus_note:" then
+      string_separated[k] = "\15"
+    end
+    if v == ":up_arrow:" then
+      string_separated[k] = "\24"
+    end
+    if v == ":down_arrow:" then
+      string_separated[k] = "\25"
+    end
+    if v == ":right_arrow:" then
+      string_separated[k] = "\26"
+    end
+    if v == ":left_arrow:" then
+      string_separated[k] = "\27"
+    end
+    if v == ":deg:" or v == ":degree:" then
+      string_separated[k] = "\176"
+    end
+  end
+  -- Stick the string back together
+  for k,v in ipairs(string_separated) do
+    parsed_string = parsed_string.." "..v
+  end
+  return parsed_string
+end
+
 function getMessages()
   local msgs_bak = net.getMessages(server, username)
   if msgs_bak == nil then
     term.setTextColor(colors.red)
     attempt = attempt + 1
     term.setTextColor(colors.white)
-    if attempt >= 3 then error("Could not connect to "..server) end
+    return false
   else
     msgs = msgs_bak
     status = server
+    return true
   end
 end
 function getServerList()
@@ -84,7 +135,7 @@ function draw()
       term.setTextColor(colors.white)
       write(">: ")
       term.setTextColor(tonumber(v.msgcolor))
-      print(v.message)
+      print(emoteParse(v.message))
       term.setTextColor(colors.white)
       local cx, cy = term.getCursorPos()
       if cy >= 17 then
@@ -103,6 +154,11 @@ function draw()
   if menu == "serverlist" then
     term.clear()
     term.setCursorPos(1,1)
+    term.setTextColor(colors.gray)
+    print(string.rep("-",mx))
+    print(" Official Servers ")
+    print(string.rep("-",mx))
+    term.setTextColor(colors.white)
     for k,v in pairs(serverList) do
 
       if select == k then
@@ -128,9 +184,11 @@ function draw()
           end
         end
         term.setTextColor(colors.white)
-        print(v.name.." ("..v.address..")")
+        print(emoteParse(v.name).." ("..v.address..")")
         if v.official then
           term.setTextColor(colors.gray)
+          print(string.rep("-",mx))
+          print(" Community Servers ")
           print(string.rep("-",mx))
           term.setTextColor(colors.white)
         else
@@ -149,6 +207,9 @@ function draw()
 
       end
     end
+    term.setCursorPos((mx/2)-(string.len( emoteParse("[enter] to select | [ :up_arrow: / :down_arrow: ] to move") )/2),my)
+    term.setTextColor(colors.white)
+    write(emoteParse("[enter] to select | [ :up_arrow: / :down_arrow: ] to move"))
   end
 
 end
@@ -197,7 +258,7 @@ function message()
 
 end
 function clicks()
-  local t = os.startTimer(10)
+  local t = os.startTimer(3)
   draw()
   while(true) do
     local event, button, x, y = os.pullEvent()
@@ -205,6 +266,8 @@ function clicks()
       if event == "key" and button == keys.enter then
         draw()
         message()
+        getMessages()
+        draw()
       end
       if event == "key" and button == keys.home then
         menu = "serverlist"
@@ -213,6 +276,8 @@ function clicks()
       if event == "mouse_click" and x >= 1 and x <= 46 and y == my-1 then
         draw()
         message()
+        getMessages()
+        draw()
       end
       if event == "timer" and button == t then
         getMessages()
@@ -224,27 +289,40 @@ function clicks()
 
         if select > 1 then
           select = select - 1
+        else
+          select = #serverList
         end
         draw()
       end
       if event == "key" and button == keys.down then
         if select < #serverList then
           select = select + 1
+        else
+          select = 1
         end
         draw()
       end
       if event == "key" and button == keys.enter then
         if select ~= #serverList then
           server = serverList[select].address
-          menu = "chat"
         else
           server = textField(my-3)
-          menu = "chat"
         end
         term.setTextColor(colors.gray)
         term.setBackgroundColor(colors.black)
         term.setCursorPos(2, my-1)
         write("Connecting... Please be patient")
+        local ok = getMessages()
+        if ok then
+          write(".. Connected!")
+          sleep(1)
+          menu = "chat"
+          draw()
+        else
+          term.setTextColor(colors.red)
+          write(".. Failed")
+          term.setTextColor(colors.gray)
+        end
       end
       if event == "key" and button == keys.home then
         shell.exit()
@@ -254,13 +332,12 @@ function clicks()
   end
 end
 function timer()
-  t = os.startTimer(10)
+  t = os.startTimer(3)
   while(true) do
     local event, tid = os.pullEvent("timer")
-    if event == "timer" and tid == t and not typing then
-      t = os.startTimer(10)
+    if event == "timer" and tid == t then
+      t = os.startTimer(3)
       getMessages()
-      draw()
     end
   end
 
